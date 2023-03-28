a2b3d92fbe28a4465daaaa9e213eb8adbc4b074a
Yubraj <imyubraz@gmail.com>
2023-03-28 08:10:41 +0545

    GET request configured to fetch all data from database (MongoDB Atlas)

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -19,10 +19,22 @@ cloudinary.config({
 
 // handeling GET request for contact/
 router.get('/',(req, res, next)=>{
-    res.status(200).json({
-        "success": true,
-        "message": "GET request on contact route"
-    });
+    Contact.find()
+    .then(result=>{
+        res.status(200).json({
+            success: true,
+            message: "Data fetched successdfully!",
+            data: result
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Error occured!",
+            error: err
+        })
+    })
 });
 
 // handeling POST request for contact/
